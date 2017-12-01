ec415daa6a5f03575b718d66255a612b23554680
Zachary Oldham <zacharyoldham@gmail.com>
2017-12-01 12:18:25 -0600

    Fixed stuff

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,11 +1,11 @@
 public class GameOfLife {
+	private boolean[][] grid = new boolean[0][0];
 	public static void main(String[] args) {
-		private boolean[][] grid = boolean[0][0];
-		// TODO: Method to draw grid
-		// TODO: Driving main method
-		// TODO: Method to return the next 'frame' of the game
-		// TODO: Method to determine whether a space should be alive or dead next frame
-		// TODO: Method to get user input
-
 	}
+
+	// TODO: Method to draw grid
+	// TODO: Driving main method
+	// TODO: Method to return the next 'frame' of the game
+	// TODO: Method to determine whether a space should be alive or dead next frame
+	// TODO: Method to get user input
 }
