c938b004aa4a21194e3e0b38900ab33bae777804
Zachary Oldham <zacharyoldham@gmail.com>
2017-12-01 12:12:32 -0600

    Method outlines

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,8 +1,11 @@
 public class GameOfLife {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		private boolean[][] grid = boolean[0][0];
+		// TODO: Method to draw grid
+		// TODO: Driving main method
+		// TODO: Method to return the next 'frame' of the game
+		// TODO: Method to determine whether a space should be alive or dead next frame
+		// TODO: Method to get user input
 
 	}
-
 }
