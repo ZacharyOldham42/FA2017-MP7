20a67c87f14f77ff295a4939b8afeb7d8dde8fb6
Zachary Oldham <zacharyoldham@gmail.com>
2017-12-01 12:33:09 -0600

    Outlined methods

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,11 +1,26 @@
 public class GameOfLife {
 	private boolean[][] grid = new boolean[0][0];
+	private int height = 0;
+	private int width = 0;
+	// TODO: Driving main method
 	public static void main(String[] args) {
 	}
 
 	// TODO: Method to draw grid
-	// TODO: Driving main method
+	public void drawGrid(boolean[][] grid) {
+		
+	}	
 	// TODO: Method to return the next 'frame' of the game
+	public boolean[][] getNextFrame(boolean[][] grid) {
+		return grid;
+	}
 	// TODO: Method to determine whether a space should be alive or dead next frame
+	public boolean isAlive(boolean[][] grid, int x, int y) {
+		return false;
+	}
 	// TODO: Method to get user input
+	public void getUserInput() {
+		
+	}
+
 }
